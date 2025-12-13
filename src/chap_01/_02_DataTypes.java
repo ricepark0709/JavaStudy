cba7d2a07b19e0db9b7f551fd0181f9c25bcf066
ricepark0709 <juncomputer0709@gmail.com>
2025-12-13 22:39:44 +0900

    자료형

---
+++ src/chap_01/_02_DataTypes.java
@@ -0,0 +1,13 @@
+package chap_01;
+
+public class _02_DataTypes {
+    public static void main(String[] args) {
+        System.out.println("안녕하세요");
+        System.out.println(12);
+        System.out.println(3.14);
+        System.out.println(-10);
+        System.out.println(true);
+        System.out.println(false);
+        System.out.println(12+12);
+    }
+}
