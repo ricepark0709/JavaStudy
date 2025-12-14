ecf683c5666a07eceae7ea4ac6d883af504621a2
ricepark0709 <juncomputer0709@gmail.com>
2025-12-14 13:32:14 +0900

    주석

---
+++ src/chap_01/_04_Comment.java
@@ -0,0 +1,15 @@
+package chap_01;
+
+public class _04_Comment {
+    public static void main(String[] args) {
+        System.out.println("잠시 후 입장이 시작됩니다."); // 10분 전
+        System.out.println("이제 경기가 시작됩니다."); // 경기 시작
+
+        /*int a = 10;
+        float b = 0.123123123123F;
+        System.out.println(a+b);*/
+
+        // 한 줄 주석 단축키 : Ctrl + /
+        // 여러 줄 주석 단축키 : Ctrl + Shift + /
+    }
+}
