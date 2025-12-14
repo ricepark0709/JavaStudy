68b4c70803d5277f1949f98e5dcdff1f263db70d
ricepark0709 <juncomputer0709@gmail.com>
2025-12-14 13:11:50 +0900

    변수 추가

--- src/chap_01/_03_Variables.java
+++ src/chap_01/_03_Variables.java
@@ -13,5 +13,12 @@ public class _03_Variables {
         System.out.println("선수 이름은 " + name + ", 타석은 " + bat + "이다.");
         System.out.println("안타 갯수는 " + hit + ", OPS는 " + OPS + ".");
         System.out.println("수비 여부는 " + field);
+
+        long salary = 3_400_000_000L;
+        float f = 0.333333333333F;
+        double d = 0.333333333333;
+        System.out.println("FA 금액 " + salary + "원");
+        System.out.println("타율 " + f);
+        System.out.println("타율 " + d);
     }
 }
