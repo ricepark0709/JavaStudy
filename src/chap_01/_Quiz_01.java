926d58a1ab559270b5ab7e231c8f4f5221b9f3af
ricepark0709 <juncomputer0709@gmail.com>
2025-12-17 15:39:14 +0900

    퀴즈

---
+++ src/chap_01/_Quiz_01.java
@@ -0,0 +1,13 @@
+package chap_01;
+
+public class _Quiz_01 {
+    public static void main(String[] args) {
+        String busNumber="상암08";
+        int leftTime=3;
+        double leftDistance=1.2;
+
+        System.out.println(busNumber+"번 버스");
+        System.out.println("약 "+leftTime+"분 후 도착");
+        System.out.println("남은 거리 "+leftDistance+"km");
+    }
+}
