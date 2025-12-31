bd09b19c0b19342c93b8bef4f60d2e27a5f22e04
ricepark0709 <juncomputer0709@gmail.com>
2025-12-31 14:58:04 +0900

    SwitchCase

---
+++ src/chap_04/_04_SwitchCase.java
@@ -0,0 +1,70 @@
+package chap_04;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class _04_SwitchCase {
+    public static void main(String[] args) {
+        // Switch Case
+
+        // 석차에 따른 장학금 지급
+        // 1등 : 전액 장학금
+        // 2등 : 반액 장학금
+        // 3등 : 반액 장학금
+        // 그 외 : 장학금 대상 아님
+
+        int ranking = 1; // 1등
+        if (ranking == 1){
+            System.out.println("전액 장학금");
+        } else if (ranking == 2 || ranking == 3) {
+            System.out.println("반액 장학금");
+        } else {
+            System.out.println("장학금 대상 아님");
+        }
+        System.out.println("조회 완료 #1");
+
+        ranking = 2;
+        switch (ranking) {
+            case 1:
+                System.out.println("전액 장학금");
+                break;
+            case 2:
+                System.out.println("반액 장학금");
+                break;
+            case 3:
+                System.out.println("반액 장학금");
+                break;
+            default:
+                System.out.println("장학금 대상 아님");
+        }
+        System.out.println("조회 완료 #2");
+
+        ranking = 3;
+        switch (ranking){
+            case 1:
+                System.out.println("전액 장학금");
+                break;
+            case 2:
+            case 3:
+                System.out.println("반액 장학금");
+                break;
+            default:
+                System.out.println("장학금 대상 아님");
+        }
+        System.out.println("조회 완료 #3");
+
+        // 중고 상품 등급 가격 매기기 (최상 : 1급, 최하 : 4급)
+        int grade = 1;
+        int price = 7000;
+
+        switch (grade){
+            case 1:
+                price += 1000;
+            case 2:
+                price += 1000;
+            case 3:
+                price += 1000;
+                break;
+        }
+        System.out.println(grade + "등급 가격의 상품은: " + price + "원");
+    }
+}
