0cdef14e2a531b148caa8527d4082c749314d7e7
ricepark0709 <juncomputer0709@gmail.com>
2026-01-02 18:33:36 +0900

    DoWhile

---
+++ src/chap_04/_07_DoWhile.java
@@ -0,0 +1,34 @@
+package chap_04;
+
+public class _07_DoWhile {
+    public static void main(String[] args) {
+        // 반복문 DoWhile
+        int distance = 25;
+        int move = 0;
+        int height = 2;
+        while (move + height < distance) {
+            System.out.println("발차기를 계속 합니다.");
+            System.out.println("현재 이동 거리 : " + move);
+            move += 3;
+        }
+        System.out.println("도착했습니다.");
+
+        // 키가 엄청나게 큰 사람?
+        move = 0;
+        height = 25;
+        while (move + height < distance) {
+            System.out.println("발차기를 계속 합니다.");
+            System.out.println("현재 이동 거리 : " + move);
+            move += 3;
+        }
+        System.out.println("도착했습니다.");
+
+        // Do While 반복문
+        do {
+            System.out.println("발차기를 합니다.");
+            System.out.println("현재 이동 거리 : " + move);
+            move += 3;
+        } while (height + move < distance);
+        System.out.println("도착했습니다.");
+    }
+}
