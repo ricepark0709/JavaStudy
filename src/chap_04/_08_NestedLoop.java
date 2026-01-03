ea6da836ce5f35c47c896af5228a8027d899b6cd
ricepark0709 <juncomputer0709@gmail.com>
2026-01-03 18:22:10 +0900

    이중반복문

--- src/chap_04/_08_NestedLoop.java
+++ src/chap_04/_08_NestedLoop.java
@@ -19,5 +19,16 @@ public class _08_NestedLoop {
             }
             System.out.println();
         }
+
+        // 별(*) 오른쪽 삼각형 만들기
+        for (int i = 0; i < 5; i++) {
+            for (int j = 4; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
