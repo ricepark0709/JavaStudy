0d6e9264958a98a3eb19f5ce0a46cadf23de42f2
ricepark0709 <juncomputer0709@gmail.com>
2026-01-07 16:59:19 +0900

    다차원 배열, 다차원 배열 순회

--- src/chap_05/_03_MultiArray.java
+++ src/chap_05/_03_MultiArray.java
@@ -8,10 +8,22 @@ public class _03_MultiArray {
         String[] seatB = {"B1", "B2", "B3", "B4", "B5"};
         String[] seatC = {"C1", "C2", "C3", "C4", "C5"};
 
-        String[][] seats = new String[][] {
+        String[][] seats = new String [][]{
                 {"A1", "A2", "A3", "A4", "A5"},
                 {"B1", "B2", "B3", "B4", "B5"},
                 {"C1", "C2", "C3", "C4", "C5"}
         };
+
+        System.out.println(seats[1][1]); // B2
+        System.out.println(seats[2][4]); // C5
+
+        String[][] seats2 = {
+                {"A1", "A2", "A3"},
+                {"B1", "B2", "B3", "B4"},
+                {"C1", "C2", "C3", "C4", "C5"}
+        };
+
+        System.out.println(seats2[0][2]); // A3
+        // System.out.println(seats2[0][4]); // A5
     }
 }
